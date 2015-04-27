a42e7dc2134362809553f2c5268769c4b58e4b96
lukawanjohi <luqato@gmail.com>
2015-04-27 12:47:19 +0300

    commit #1 - Solution outline

--- R/gemlist_package.R
+++ R/gemlist_package.R
@@ -1,1 +1,9 @@
 #' gemlist-package#'#' A Good Title#'#' A thorough description#'#' @name gemlist-package#' @docType packageNULL    
+#Check existance of prerequisite packages and install if missing
+#user downloads template with data
+#user uploads .xls or .xlsx with gerplasm list
+#list rendered in r
+#list loaded into a dataframe called gemlist
+#germlist dataframe contains Entry, Name, Pedigree and Vines_per_plot
+#variables
+file_path <- function
